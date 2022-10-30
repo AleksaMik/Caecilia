5ea68d4f0e01d173a8d3340f70035b67ef68c084
AleksaMik <aleksam.2509@gmail.com>
2022-10-30 12:53:17 -0500

    index.html upd

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 // serviceWorkerRegistration.unregister();
 
 // reportWebVitals();
