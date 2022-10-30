56b114f49ee96634a7497702ab4c28076ea19676
AleksaMik <aleksam.2509@gmail.com>
2022-10-30 14:03:45 -0500

    index.html/components/header upd

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // ReactDOM.render(
@@ -13,6 +15,8 @@ ReactDOM.render(<App />, document.getElementById('root'));
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
+
+
 // serviceWorkerRegistration.unregister();
 
 // reportWebVitals();
