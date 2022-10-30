145ba5da3325bfe752a5e4b1d9a0f11a5c6e33a4
AleksaMik <aleksam.2509@gmail.com>
2022-10-30 13:35:01 -0500

    App.js upd

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
-import "./App.css";
-import React, { useState } from "react";
-import Header from "./components/header.js";
-import Footer from "./components/footer.js";
-import About from "./components/about.js";
-import Portfolio from "./components/portfolio.js";
-import Contact from "./components/contact.js";
-import Resume from "./components/resume.js";
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
-	const [currentTab, setCurrentTab] = useState("about");
-	const renderTab = () => {
-		switch (currentTab) {
-			case "about":
-				return <About />;
-			case "portfolio":
-				return <Portfolio />;
-			case "contact":
-				return <Contact />;
-			case "resume":
-				return <Resume />;
-			default:
-				return null;
-		}
-	};
-
-	return (
-		<div>
-			<div className="mobile-header">
-				<Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
-			</div>
-			<div>
-				<main>{renderTab()}</main>
-			</div>
-			<div>
-				<Footer></Footer>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <Header />
+      <Footer/>
+    </div>
+  );
 }
 
 export default App;
