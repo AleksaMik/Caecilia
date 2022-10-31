534f3506098dd687a38f27bd3f06b6289e92aea4
AleksaMik <aleksam.2509@gmail.com>
2022-10-30 19:30:18 -0500

    favicon.ico

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,7 +10,7 @@ import Resume from '../../components/Resume';
 class Header extends Component {
   render() {
     return (
-      <HashRouter>
+      <Navigate>
         <header>
           <Navigation />
         </header>
@@ -23,7 +23,7 @@ class Header extends Component {
           <Route path="/resume" component={Resume}/>
           <Route path="/" element={<Navigate to= "/" />}/>
         </div>
-      </HashRouter>
+      </Navigate>
     );
   }
 }
