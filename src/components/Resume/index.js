786fa9597e949d703ffed142ca779501165a89a1
AleksaMik <aleksam.2509@gmail.com>
2022-11-01 11:06:54 -0500

    resume added

---
+++ src/components/Resume/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Resume() {
+  return (
+    <section class="container">
+      <h2 className="top-title">Resume</h2>
+      <hr></hr>
+
+      <div>
+        <div class="mt-5">
+          <h2 className="top-title">Aleksandra Mikhailichenko</h2>
+          <ul>
+            <li>
+            Aliquam ut porttitor leo a diam sollicitudin tempor id eu. Nulla aliquet enim tortor at. Eu facilisis sed odio morbi quis commodo odio aenean sed. Tincidunt id aliquet risus feugiat in. Mi eget mauris pharetra et ultrices neque ornare aenean euismod. Sed elementum tempus egestas sed sed risus pretium. Commodo elit at imperdiet dui. Pharetra sit amet aliquam id diam. Facilisi cras fermentum odio eu feugiat. Interdum velit laoreet id donec ultrices.
+            </li>
+          </ul>
+          <p class="mt-5">
+            <a href="www.linkedin.com/in/aleksandra-mikhailichenko-50782566">
+              <img
+                src="https://img.icons8.com/fluent/48/000000/linkedin.png"
+                alt="linkedIn"
+              />
+            </a>
+          </p>
+
+          <a
+            href="https://github.com/AleksaMik"
+            class="link"
+          >
+          </a>
+        </div>
+      </div>
+
+      <div class="justify-content-center mt-5">
+        <div>
+          <h2 className="top-title">Proficiencies</h2>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Resume;
