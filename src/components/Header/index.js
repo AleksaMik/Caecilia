dc60ffaeb7b7938ec67b63770b61ebe877a4084a
AleksaMik <aleksam.2509@gmail.com>
2022-11-01 11:27:43 -0500

    components/portfolio upd

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Route, Redirect, HashRouter } from "react-router-dom";
 import Navigation from "../../components/Navigation";
 import About from "../../components/About";
-import Portfolio from "../../components/Portfolio";
+import Portfolio from "../Resume";
 import Contact from '../../components/Contact';
 // import Resume from '../../components/Resume';
 
